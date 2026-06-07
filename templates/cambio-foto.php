bdabfa96dc84e5e6938044538b5927c9fd197189
Fatima Rosales <fm455878@gmail.com>
2026-06-06 21:36:24 -0600

    feat(cambio-foto):se agregan los cambios para añadir la foto

---
+++ templates/cambio-foto.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="autor" content="Equipo 4: StatHorses">
+    <meta name="description" content="Mi página de encabezado">
+    <link rel="stylesheet" href="../statics/style/barra-busqueda-head.css">
+    <link rel="stylesheet" href="../statics/style/cambio-foto.css">
+    
+    <title>SAETEC</title>
+</head>
+<body>
+    <!<!-------------------------------------BARRA DE BUSQUEDA----------------------------------------->
+        <header>
+            <div id="iconos_unam">
+                <img class="iconos" src="../statics/img/logo-escudo-unam.png" alt="Escudo de la UNAM">
+                <img class="iconos" src="../statics/img/logo_enp.jpeg" alt="Escudo de la UNAM">
+                <img class="iconos" src="../statics/img/logo-prepa6.png" alt="Escudo de la UNAM">
+                <img class="iconos" src="../statics/img/logo-475años.png" alt="Escudo de la UNAM">
+            </div>
+            <div id="titulo_encabezado">
+                <p>SAETEC</p>
+            </div>
+            <div id="iconos_ete">
+                <img class="iconos" src="../statics/img/logo_compu.jpeg" alt="Escudo de el Estudio Tecnico Especializado en Computacion">
+                <img class="iconos" src="../statics/img/logo-ete.png" alt="Escudo de los Estudios Tecnicos de la UNAM">
+                <img class="iconos" src="../statics/img/logo-ete-40años.png" alt="Escudo de los 40 años Estudios Tecnicos de la UNAM"> 
+            </div>
+        </header>
+        <!------------------------BARRA DE NAVEGACIÓN--------------------------------->
+        <nav class="nav">
+            <div class="container">
+                <div class="btn-menu">
+                    <label for="btn-menu">☰</label>
+                </div>
+                <nav class="menu">
+                    <a href="./index.html">Inicio</a>
+                    <a href="#">Perfil</a>
+                    <a href="#">Acerca</a>
+                    <a href="#">Contacto</a>
+                </nav>
+            </div>
+        </nav>
+        <!-------------------------------------BARRA LATERAL----------------------------------------->
+        <aside>
+            <div class="capa"> </div>
+            <!--  --------------->
+            <input type="checkbox" id="btn-menu"/>
+            <div class="container-menu">
+                <div class="cont-menu">
+                    <nav>
+                        <h2>324308734</h2>
+                        <a href="./perfil-alumno.html">Mi perfil</a>
+                        <hr>
+                        <a href="#">Mis calificaciones</a>
+                        <hr>
+                        <a href="#">Mis actividades</a>
+                        <hr>
+                        <a href="#">¿Cómo te has sentido?</a>
+                        <hr>
+                        <a href="../docs/2019_Acuerdo_Rectoria_02.pdf">Políticas de seguridad</a>
+                        <hr>
+                        <a href="#">Modificar datos</a>
+                    </nav>
+                    <label for="btn-menu">✖️</label>
+                </div>
+            </div>
+        </aside>
+        <!-------------------------------------CAMBIO DE FOTO----------------------------------------->
+        <div class="contenedor-general" style="margin-top: 50px;">
+            <header class="form-header">
+                <h2>Edición de Perfil</h2>
+            </header>
+            <div class="contenedor-foto-perfil">
+
+            <!-- Aquí va el PHP para mostrar la foto de perfil actual en grande-->
+
+            </div>
+            <!-- otro atributo? -->
+            <form action="guardar.php" method="POST" enctype="multipart/form-data">
+
+                <input type="file" name="foto_perfil" id="ipt-foto_perfil" accept="image/png, image/jpeg">
+
+                <button type="submit" class="btn-submit">Cambiar foto</button>
+            </form>
+        </div>
+</body>
+</html>
