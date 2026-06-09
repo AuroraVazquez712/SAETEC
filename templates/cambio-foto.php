b9490981efd6d699581595ae6cd6d3db980e4942
Fatima Rosales <fm455878@gmail.com>
2026-06-08 21:08:57 -0600

    feat(cambio-foto):se agregan cambios

--- templates/cambio-foto.php
+++ templates/cambio-foto.php
@@ -28,14 +28,45 @@
                 <img class="iconos" src="../statics/img/logo-ete-40años.png" alt="Escudo de los 40 años Estudios Tecnicos de la UNAM"> 
             </div>
         </header>
-        <!------------------------BARRA DE NAVEGACIÓN------------------------>
-    <?php
-            include 'barrapro.php';
-    ?>
-    <!-------------------------------------BARRA LATERAL----------------------------------------->
-    <?php
-        include 'barra-lateral.php';
-    ?>
+        <!------------------------BARRA DE NAVEGACIÓN--------------------------------->
+        <nav class="nav">
+            <div class="container">
+                <div class="btn-menu">
+                    <label for="btn-menu">☰</label>
+                </div>
+                <nav class="menu">
+                    <a href="./index.html">Inicio</a>
+                    <a href="#">Perfil</a>
+                    <a href="#">Acerca</a>
+                    <a href="#">Contacto</a>
+                </nav>
+            </div>
+        </nav>
+        <!-------------------------------------BARRA LATERAL----------------------------------------->
+        <aside>
+            <div class="capa"> </div>
+            <!--  --------------->
+            <input type="checkbox" id="btn-menu"/>
+            <div class="container-menu">
+                <div class="cont-menu">
+                    <nav>
+                        <h2>324308734</h2>
+                        <a href="./perfil-alumno.html">Mi perfil</a>
+                        <hr>
+                        <a href="#">Mis calificaciones</a>
+                        <hr>
+                        <a href="#">Mis actividades</a>
+                        <hr>
+                        <a href="#">¿Cómo te has sentido?</a>
+                        <hr>
+                        <a href="../docs/2019_Acuerdo_Rectoria_02.pdf">Políticas de seguridad</a>
+                        <hr>
+                        <a href="#">Modificar datos</a>
+                    </nav>
+                    <label for="btn-menu">✖️</label>
+                </div>
+            </div>
+        </aside>
         <!-------------------------------------CAMBIO DE FOTO----------------------------------------->
         <div class="contenedor-general" style="margin-top: 50px;">
             <header class="form-header">
@@ -53,7 +84,6 @@
                     $archivo = $_FILES['foto_perfil'];
                     $nombre_archivo = $archivo['name'];
                     $ruta_temporal = $archivo['tmp_name'];
-
                     move_uploaded_file($ruta_temporal, '../statics/img/perfil_usuario.jpg');
                 }
             ?>
@@ -71,9 +101,5 @@
                 <a href="perfil-alumno.php" class="btn-editar">Regresar al perfil</a>
             </div>
         </div>
-        <!------------------------FOOTER --------------------------------->
-        <?php
-                include 'footer.php';
-        ?> 
 </body>
 </html>
