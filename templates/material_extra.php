7151f405b2a91e93da7c0d673fc9a9cc388d5507
AuroraVazquez712 <324308734@alumno.enp.unam.mx>
2026-06-09 02:09:31 -0600

    feat(material extra):agrega link a bibliografía y videos pra modulo 3, 4 y 5

--- templates/material_extra.php
+++ templates/material_extra.php
@@ -67,7 +67,7 @@
     <main>
         <div id="materiales">
             <div id="modulo">
-                <div class="titulo_modulo">Módulo 1</div>
+                <div class="titulo_modulo">Módulo 1: Aplicaciones de uso general</div>
                 <div class="ejercicio">
                     <h3> Ejercicios: </h3>
                         <p>Aquí va el ejercicio UwU</p>
@@ -78,10 +78,17 @@
                 </div>
             </div>
             <div id="modulo">
-                <div class="titulo_modulo">Módulo 2</div>
+                <div class="titulo_modulo">Módulo 2: Introducción a la Computación</div>
                 <div class="ejercicio">
-                    <h3> Ejercicios: </h3>
-                        <p>Aquí va el ejercicio UwU</p>
+                    <h3> Bibliografía: </h3>
+                        <ul>
+                            <li>Vasconcelos, J.  (2013). 
+                                Introducción a la computación. México: Grupo Editorial Patria. Página: 296 </li>
+                            <li><a href="https://www.mheducation.es/bcv/guide/capitulo/8448147227.pdf">
+                                Introducción a la programación en C.</a> </li>
+                            <li><a href="http://fcasua.contad.unam.mx/apuntes/interiores/docs/2005/informatica/1/1167.pdf">
+                                Kanagusico, D. (s.f.). Apunte Introducción a la Programación. Páginas: 4-5 </a></li>
+                        </ul>
                 </div>
                 <div class="video">
                     <h3>Videos de Apoyo:</h3>
@@ -89,10 +96,38 @@
                 </div>
             </div>
             <div id="modulo">
-                <div class="titulo_modulo">Módulo 3</div>
+                <div class="titulo_modulo">Módulo 3: Programación Estructurada</div>
                 <div class="ejercicio">
-                    <h3> Ejercicios: </h3>
-                        <p>Aquí va el ejercicio UwU</p>
+                    <h3> Bibliografía: </h3>
+                        <ul>
+                            <li> Deitel, H. y Deitel, P. (2004). Cómo programar en C, C++ y Java. México: 
+                                Prentice Hall. Páginas: 51-69, 90-106 </li>
+                            <li><a href="https://webs.um.es/iverdu/P00LibreriasANSIc.pdf ">
+                                Davidson, S. y Pozo, S. (2003). Librerías ANSI C: Librerías estándar C. R.</a> </li>
+                            <li><a href="http://odin.fi-b.unam.mx/salac/practicasFP/fp_p7.pdf ">
+                                García, E. y Solano, J. (s.f.). Guía práctica de estudio 07: Fundamentos de Lenguaje C. </a></li>
+                        </ul>
+                </div>
+                <div class="video">
+                    <h3>Videos de Apoyo:</h3>
+                        <ul>
+                            <li><a href="https://www.youtube.com/watch?v=rEsSxd0L4GI&list=PLpOqH6AE0tNgqknxjMAJ8bX_L1a7lnBaH">
+                                Codigofacilito (s.f.). Curso Básico de C [Archivos de video]. </a> </li>
+                            <li><a href="https://www.youtube.com/watch?v=9ijhlPfxFnk&list=PLMDLYpoZkTxNARxzB3-FcqcXoEOExyxe7">
+                                Codeando. (2015). Curso de C [Archivos de video] </a></li>
+                            <li><a href="https://www.youtube.com/watch?v=ztEsa-dtn3E">
+                                Casanova, R. (2012). Manejo de archivos en lenguaje C [Archivo de video].</a></li>
+                        </ul>
+                </div>
+            </div>
+            <div id="modulo">
+                <div class="titulo_modulo">Módulo 4: Sistemas Operativos</div>
+                <div class="ejercicio">
+                    <h3> Bibliografía: </h3>
+                        <ul>
+                            <li><a href="https://triton.astroscu.unam.mx/fruiz/introduccion/clases/so/SO.pdf">
+                                Ruiz Sala, F. (s. f.). Sistema operativo. Instituto de Astronomía, Universidad Nacional Autónoma de México.</a></li>
+                        </ul>
                 </div>
                 <div class="video">
                     <h3>Videos de Apoyo:</h3>
@@ -100,25 +135,14 @@
                 </div>
             </div>
             <div id="modulo">
-                <div class="titulo_modulo">Módulo 4</div>
+                <div class="titulo_modulo">Módulo 5: Solución de problemas y tecns. progr.</div>
                 <div class="ejercicio">
                     <h3> Ejercicios: </h3>
                         <p>Aquí va el ejercicio UwU</p>
                 </div>
                 <div class="video">
                     <h3>Videos de Apoyo:</h3>
-                        <p>Link al super video</p>
-                </div>
-            </div>
-            <div id="modulo">
-                <div class="titulo_modulo">Módulo 5</div>
-                <div class="ejercicio">
-                    <h3> Ejercicios: </h3>
-                        <p>Aquí va el ejercicio UwU</p>
-                </div>
-                <div class="video">
-                    <h3>Videos de Apoyo:</h3>
-                        <p>Link al super video</p>
+                        
                 </div>
             </div>
         </div>
