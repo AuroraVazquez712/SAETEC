76e381ad8bc1f128d770bdaccd9f0a2fc4d8be18
Fatima Rosales <fm455878@gmail.com>
2026-06-09 07:40:30 -0600

    fix(cambio-foto):se arreglan conflictos

--- templates/cambio-foto.php
+++ templates/cambio-foto.php
@@ -49,7 +49,6 @@
                     <img class="iconos" src="../statics/img/logo-ete.png" alt="Escudo de los Estudios Tecnicos de la UNAM">
                 </a>
             </div>
-<<<<<<< HEAD
             <div class="logo-40años"></div>
                     <img class="iconos" src="../statics/img/logo-ete-40años.png" alt="Escudo de los 40 años Estudios Tecnicos de la UNAM"> 
             </div>
@@ -63,48 +62,6 @@
     <?php
         include 'barra-lateral.php';
     ?>
-=======
-        </header>
-        <!------------------------BARRA DE NAVEGACIÓN--------------------------------->
-        <nav class="nav">
-            <div class="container">
-                <div class="btn-menu">
-                    <label for="btn-menu">☰</label>
-                </div>
-                <nav class="menu">
-                    <a href="./index.html">Inicio</a>
-                    <a href="#">Perfil</a>
-                    <a href="#">Acerca</a>
-                    <a href="#">Contacto</a>
-                </nav>
-            </div>
-        </nav>
-        <!-------------------------------------BARRA LATERAL----------------------------------------->
-        <aside>
-            <div class="capa"> </div>
-            <!--  --------------->
-            <input type="checkbox" id="btn-menu"/>
-            <div class="container-menu">
-                <div class="cont-menu">
-                    <nav>
-                        <h2>324308734</h2>
-                        <a href="./perfil-alumno.html">Mi perfil</a>
-                        <hr>
-                        <a href="#">Mis calificaciones</a>
-                        <hr>
-                        <a href="#">Mis actividades</a>
-                        <hr>
-                        <a href="#">¿Cómo te has sentido?</a>
-                        <hr>
-                        <a href="../docs/2019_Acuerdo_Rectoria_02.pdf">Políticas de seguridad</a>
-                        <hr>
-                        <a href="#">Modificar datos</a>
-                    </nav>
-                    <label for="btn-menu">✖️</label>
-                </div>
-            </div>
-        </aside>
->>>>>>> 7efd7db497d63582c0dd4c6bf43f06a66afce988
         <!-------------------------------------CAMBIO DE FOTO----------------------------------------->
         <div class="contenedor-general" style="margin-top: 50px;">
             <header class="form-header">
