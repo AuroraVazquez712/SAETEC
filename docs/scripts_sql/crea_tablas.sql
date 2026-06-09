83e6af3a99af65c8145b8d55d59383d97f90c284
Diego Ivan Salcedo Perez <325156992@alumno.enp.unam.mx>
2026-06-08 20:48:15 -0600

    docs(crea_tablas): agregar las tablas faltantes

--- docs/scripts_sql/crea_tablas.sql
+++ docs/scripts_sql/crea_tablas.sql
@@ -71,3 +71,149 @@ VALUES (1, 4, "325156992");
 
 INSERT INTO estudiante (id_estudiante, id_habito, nocta)
 VALUES (2, 2, "325295949");
+
+
+-- TABLA DIFICULTAD
+CREATE TABLE dificultad (
+    id_dificultad INT PRIMARY KEY AUTO_INCREMENT,
+    nombre_dificutad VARCHAR(30)
+);
+
+INSERT INTO dificultad (nombre_dificutad)
+VALUES ("tiempo"),
+("desconocimiento"),
+("falta de computadora"),
+("grupo demandante"),
+("Motivos personales");
+
+
+-- TABLA RAZÓN DE INGRESO
+CREATE TABLE razon_ingreso (
+    id_razon INT PRIMARY KEY AUTO_INCREMENT,
+    nombre_razon VARCHAR(20)
+);
+
+INSERT INTO razon_ingreso (nombre_razon)
+VALUES ("curiosidad"),
+("interés previo"), 
+("relación con la carrera");
+
+
+-- TABLA INTERÉS
+CREATE TABLE interes (
+    id_interes INT PRIMARY KEY AUTO_INCREMENT,
+    nombre_interes VARCHAR(20)
+);
+
+INSERT INTO interes (nombre_interes)
+VALUES ("deportes"),
+("artes"), 
+("tecnología"),
+("espectáculo");
+
+
+-- TABLA COMENTARIO
+CREATE TABLE comentario (
+    id_comentario INT PRIMARY KEY AUTO_INCREMENT,
+    id_estudiante INT NULL,
+    FOREIGN KEY (id_estudiante) 
+    REFERENCES estudiante(id_estudiante),
+    comentario VARCHAR(100) NOT NULL,
+    fecha_publicacion DATE NOT NULL
+);
+
+INSERT INTO comentario 
+(id_estudiante, comentario, fecha_publicacion)
+VALUES 
+(1, "Hola mundo", "2026-06-08");
+
+
+-- TABLA RESPUESTA
+CREATE TABLE respuesta (
+    id_respuesta INT PRIMARY KEY AUTO_INCREMENT,
+    id_comentario INT NULL,
+    FOREIGN KEY (id_comentario) 
+    REFERENCES comentario(id_comentario),
+    comentario VARCHAR(100) NOT NULL,
+    fecha_publicacion DATE NOT NULL
+);
+
+INSERT INTO respuesta 
+(id_estudiante, comentario, fecha_publicacion)
+VALUES 
+(1, "Hola equipo", "2026-06-08");
+
+
+-- TABLA CUESTIONARIO
+CREATE TABLE cuestionario (
+    id_cuestionario INT PRIMARY KEY AUTO_INCREMENT,
+    id_estudiante INT NULL,
+    FOREIGN KEY (id_estudiante) 
+    REFERENCES estudiante(id_estudiante),
+    areas_oportunidad VARCHAR(40) NOT NULL,
+    sentir_estudiante VARCHAR(40) NOT NULL,
+    cuesta_trabajo VARCHAR(40) NOT NULL,
+    fecha_emision DATE NOT NULL
+);
+
+INSERT INTO cuestionario
+(id_estudiante, comentario, fecha_publicacion)
+VALUES 
+(1, "Hola mundo", "2026-06-08");
+
+
+-- TABLA ACTIVIDAD
+CREATE TABLE actividad (
+    id_actividad INT PRIMARY KEY AUTO_INCREMENT,
+    id_profesor INT NULL,
+    FOREIGN KEY (id_profesor) 
+    REFERENCES profesor(id_profesor),
+    nombre_actividad VARCHAR(30) NOT NULL,
+    descripcion VARCHAR(100) NULL,
+    fecha_entrega DATE NOT NULL,
+);
+
+INSERT INTO actividad 
+(id_profesor, nombre_actividad, fecha_entrega)
+VALUES 
+(1, "Serie de Karel","2026-06-08");
+
+-- TABLA GRUPO
+CREATE TABLE grupo (
+    id_grupo INT PRIMARY KEY AUTO_INCREMENT,
+    id_profesor INT NULL,
+    FOREIGN KEY (id_profesor) 
+    REFERENCES profesor(id_profesor),
+    nombre_grupo VARCHAR(3) NOT NULL,
+    plantel VARCHAR(50) NOT NULL,
+    cupo INT NOT NULL,
+    salon VARCHAR(6) NOT NULL,
+);
+
+INSERT INTO grupo
+(id_profesor, nombre_grupo, plantel, cupo, salon)
+VALUES 
+(1, "61B","Escuela Nacional Preparatoria No. 6 Antonio Caso", 50, "LACEC");
+
+INSERT INTO grupo
+(id_profesor, nombre_grupo, plantel, cupo, salon)
+VALUES 
+(1, "61D","Escuela Nacional Preparatoria No. 6 Antonio Caso", 50, "LACEC");
+
+
+-- TABLA ASIGNACION
+CREATE TABLE asignacion (
+    id_asignacion INT PRIMARY KEY AUTO_INCREMENT,
+    id_actividad INT NULL,
+    FOREIGN KEY (id_actividad) 
+    REFERENCES actividad(id_actividad),
+    id_estudiante INT NULL,
+    FOREIGN KEY (id_estudiante) 
+    REFERENCES estudiante(id_estudiante),
+    calificacion INT NOT NULL,
+);
+
+INSERT INTO grupo
+(id_actividad, id_estudiante, calificacion)
+VALUES 
+(1, 2, 10);
