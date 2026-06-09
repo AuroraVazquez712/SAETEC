09738d9666de29f8b6e7dfbfde99634fb0e47582
AuroraVazquez712 <324308734@alumno.enp.unam.mx>
2026-06-09 17:48:28 -0600

    feat(formalu-confirmar):Añade php para confirmar los datos recibidos del alumno

---
+++ templates/formalu-confirmar.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="autor" content="Equipo 4: Diego Ivan Salcedo Perez">
+    <meta name="description" content="Mi página de encabezado">
+    <link rel="stylesheet" href="../statics/style/formalu.css">
+    <title>Formulario del alumno</title>
+</head>
+<body>
+    <header>
+        <div id="iconos_unam">
+            <div class="logo-unam">
+                <a href="https://www.unam.mx/">
+                    <img class="iconos"src="../statics/img/logo-escudo-unam.png" alt="Escuedo de la UNAM">
+                </a>
+            </div>
+            <div class="logo-enp">
+                <a href="http://enp.unam.mx/">
+                    <img class="iconos"src="../statics/img/logo_enp.jpeg" alt="Escuedo de la UNAM">
+                </a>
+            </div>
+            <div class="logo-enp6">
+                <a href="https://www.prepa6.unam.mx/ENP6/_P6/">
+                    <img class="iconos"src="../statics/img/logo-prepa6.png" alt="Escuedo de la UNAM">
+                </a>
+            </div>
+            <div class="logo-475años">
+                <img class="iconos"src="../statics/img/logo-475años.png" alt="Escuedo de la UNAM">
+            </div>
+        </div>
+        <div id="titulo_encabezado">
+            <a href="./index.html">
+                <p>SAETEC</p>
+            </a>
+        </div>
+        <div id="iconos_ete">
+            <div class="logo-compu">
+                <a href="https://www.ete.enp.unam.mx/">
+                    <img class="iconos" src="../statics/img/logo_compu.jpeg" alt="Escudo de el Estudio Tecnico Especializado en Computacion">
+                </a>
+            </div>
+            <div class="logo-ete"></div>
+                <a href="https://www.ete.enp.unam.mx/CM.html">
+                    <img class="iconos" src="../statics/img/logo-ete.png" alt="Escudo de los Estudios Tecnicos de la UNAM">
+                </a>
+            </div>
+            <div class="logo-40años"></div>
+                    <img class="iconos" src="../statics/img/logo-ete-40años.png" alt="Escudo de los 40 años Estudios Tecnicos de la UNAM"> 
+            </div>
+        </div>
+    </header>
+    <!------------------------BARRA DE NAVEGACIÓN--------------------------------->
+    <?php
+            include 'barrapro.php';
+    ?> 
+    <br>
+    <!-------------------------------------------FORMS------------>
+    <div class="container" style="margin-top: 50px;">
+        <header class="form-header">
+            <h2>¡Alumno Registrado!</h2>
+        </header>
+
+        <div class="alumno-registro" style="background: #f8f9fa; padding: 20px; border-left: 5px solid #3f2f83; border-radius: 5px;">
+            <h3>Datos del alumno:</h3>
+            
+            <!-- Aquí va el PHP para mostrar los datos recibidos -->
+            <?php
+                $id_perfil= $_POST["id_perfil"];
+                $nombre= $_POST["nombre"];
+                $apellido_paterno= $_POST["apellidopat"];
+                $apellido_materno= $_POST["apellidomat"];
+                $correo= $_POST["correo"];
+                $fecha_nacimiento= $_POST["fecha_nacimiento"];
+                $grupo= $_POST["grupo"];
+
+                echo "<p> Perfil: $id_perfil </p>";
+                echo "<p> Nombre: $nombre</p>";
+                echo "<p> Apellido Paterno: $apellido_paterno </p>";
+                echo "<p> Apellido Materno: $apellido_materno</p>";
+                echo "<p> Correo: $correo</p>";
+                echo "<p> Fecha de nacimiento: $fecha_nacimiento</p>";
+                echo "<p> Grupo: $grupo</p>";
+            ?>
+        </div>
+        
+        <br>
+    </div>
+    <!------------------------FOOTER --------------------------------->
+    <?php
+            include 'footer.php';
+    ?> 
+</body>
+</html>
