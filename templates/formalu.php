a87e6e346554794adf45047a589ca907414e8e54
AuroraVazquez712 <324308734@alumno.enp.unam.mx>
2026-06-11 00:05:42 -0600

    feat(formalu):añade select a las opc nde grupo

--- templates/formalu.php
+++ templates/formalu.php
@@ -92,7 +92,11 @@
 
             <div class="input-group">
                 <label for="grupo">Grupo:</label>
-                <input type="text" name="grupo" id="ipt-grupo">
+                <select name="grupo" id="ipt-grupo" required>
+                    <option value="" disabled selected>Elige tu grupo...</option>
+                    <option value="61B">61B</option>
+                    <option value="61D">61D</option>
+                </select>
             </div>
 
         </div>
