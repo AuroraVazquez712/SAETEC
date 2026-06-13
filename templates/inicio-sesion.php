ec5fec1de4839b6a6f532c2cf9b15ce17686f180
Diego Ivan Salcedo Perez <325156992@alumno.enp.unam.mx>
2026-06-13 00:35:27 -0600

    docs(respaldos_bd): agrega la tabla administrador

--- templates/inicio-sesion.php
+++ templates/inicio-sesion.php
@@ -1,14 +1,3 @@
-
-<?php   
-    if(isset($_FILES['foto_perfil']))
-    {
-        $archivo = $_FILES['foto_perfil'];
-        $nombre_archivo = $archivo['name'];
-        $ruta_temporal = $archivo['tmp_name'];
-
-        move_uploaded_file($ruta_temporal, '../statics/img/perfil-usuario.jpg');
-    }
-?>
 <?php
     session_start();
 
@@ -19,16 +8,84 @@
 
         $usuario = trim($_POST["usuario"]);
         $contrasenha = trim($_POST["contrasenha"]);
+        $hasheo = password_hash($contrasenha, PASSWORD_DEFAULT);
 
         //Debe agregarse un usuario administrador, y profesor para checar credenciales
 
-        //PERFIL DE ESTUDIANTE consulta 1
+        // Primero revisamos que el registro esté en la tabla estudiante
+        // Si sí, entonces será un usuario de tipo estudiante. Y ponemos los datos 
+        // en las variables de sesión
+
+        // Si no, revisamos la tabla de profesor
+
+        // Query para bsucar si el usuario está en 'estudiante'
+        $query = "SELECT  id_estudiante, id_grupo, nocta FROM estudiante WHERE nocta = '$usuario'";
+        $result = mysqli_query($con, $query);
+        // Contamos el numero de filas devueltas
+        $num_registros = mysqli_num_rows($result);
+
+        if($num_registros > 0){
+            // El usuario es estudiante
+            // Guardaremos los datos en variables de sesion, cookies,
+            // y redirigirlo
+            $registro1 = mysqli_fetch_assoc($result);
+            $id_perfil = $registro1["id_estudiante"];
+
+            // TABLA PERFIL CONSULTA 2
+            $query2 = "SELECT id_perfil, nombre, apellido_paterno, apellido_materno, correo, contrasenha 
+            FROM perfil WHERE id_perfil = '$id_perfil'";
+            //Cambiar el contrsenha a hasheo, después de hacer el formulario
+            $result2 = mysqli_query( $con, $query2);
+            $registro2 = mysqli_fetch_assoc($result2);
+
+            //CONSULTA DE GRUPO, TABLE GRUPO
+            $id_grupo = $registro1["id_grupo"];
+            $query3 = "SELECT id_grupo, nombre_grupo FROM grupo WHERE id_grupo = '$id_grupo'";
+            $result3 = mysqli_query( $con, $query3);
+            $registro3 = mysqli_fetch_assoc($result3);
+
+            if ($registro2)
+            {
+                $_SESSION["nombre_completo"] = $registro2["nombre"] . " " . $registro2["apellido_paterno"] . " " . $registro2["apellido_materno"];
+                $_SESSION["correo"] = $registro2["correo"];
+                $_SESSION["nocta"] = $registro1["nocta"];
+                $_SESSION["grupo"] = $registro3["nombre_grupo"];
+                $_SESSION["id_perfil"] = $registro2["id_perfil"];
+                $_SESSION["rol"] = "E";
+                setcookie("usuario", $registro1["nocta"], time() + (86400)); // 1 dia = 86400 segundos, expirará en un dia
+
+                header("Location: perfil-alumno.php");
+                exit;
+            }
+        }
+
+        // Si el codigo llega aquí, no era estudiante
+        // Debemos revisar que sea profe o admin
+
+        $query = "SELECT * FROM profesor WHERE no_trabajador = '$usuario'";
+        $result = mysqli_query($con, $query);
+        $num_registros = mysqli_num_rows($result);
+        if($num_registros > 0){
+            // Sí era profesor, toca guardar sus respectivas variables de sesión, cookies,
+            // y redirigirlo
+        }
+
+        // Cuando haya admin, haremos lo mismo
+
+
+
+
+
+
+        //TABLA ESTUDIANTE CONSULTA 1
         $query1 = "SELECT nocta, id_grupo FROM estudiante WHERE nocta = '$usuario'";
         $result1 = mysqli_query( $con, $query1);
         $registro1 = mysqli_fetch_assoc($result1);
 
-        //consulta 2 para contrasenha
-        $query2 = "SELECT  nombre, apellido_paterno, apellido_materno, correo, contrasenha FROM perfil WHERE contrasenha = '$contrasenha'";
+        // TABLA PERFIL CONSULTA 2
+        $query2 = "SELECT id_perfil, nombre, apellido_paterno, apellido_materno, correo, contrasenha 
+        FROM perfil WHERE contrasenha = '$contrasenha'";
+        //Cambiar el contrsenha a hasheo, después de hacer el formulario
         $result2 = mysqli_query( $con, $query2);
         $registro2 = mysqli_fetch_assoc($result2);
 
@@ -37,21 +94,25 @@
         $query3 = "SELECT id_grupo, nombre_grupo FROM grupo WHERE id_grupo = '$id_grupo'";
         $result3 = mysqli_query( $con, $query3);
         $registro3 = mysqli_fetch_assoc($result3);
-        
 
+        
+        // Verificar ci es E, A o P
         if ($registro1 && $registro2)
         {
             $_SESSION["nombre_completo"] = $registro2["nombre"] . " " . $registro2["apellido_paterno"] . " " . $registro2["apellido_materno"];
             $_SESSION["correo"] = $registro2["correo"];
             $_SESSION["nocta"] = $registro1["nocta"];
             $_SESSION["grupo"] = $registro3["nombre_grupo"];
+            $_SESSION["id_perfil"] = $registro2["id_perfil"];
             setcookie("usuario", $registro1["nocta"], time() + (86400)); // 1 dia = 86400 segundos, expirará en un dia
+
             header("Location: perfil-alumno.php");
-            exit();
+            exit;
         } else {
             $error = "No coinciden usuario o contraseña";
         }
     } 
+
 
 ?>
 <!DOCTYPE html>
@@ -61,19 +122,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="autor" content="Equipo 4: StatHorses">
     <meta name="description" content="Mi página de encabezado">
-
-    <link rel="stylesheet" href="../statics/style/barra-busqueda-head.css">
-    <link rel="stylesheet" href="../statics/style/cambio-foto.css">
-    
-    <title>SAETEC</title>
-</head>
-<body>
-    <!<!-------------------------------------BARRA DE BUSQUEDA----------------------------------------->
-        < <header>
-        <div id="iconos_unam">
-            <div class="logo-unam">
-                <a href="https://www.unam.mx/">
-
     <link rel="stylesheet" href="../statics/style/inicio-sesion.css">
     <link rel="stylesheet" href="../statics/style/syle.css">
 
@@ -85,25 +133,16 @@
         <div id="iconos_unam">
             <div class="logo-unam">
                 <a href="https://www.unam.mx/" target="_blank">
-
                     <img class="iconos"src="../statics/img/logo-escudo-unam.png" alt="Escuedo de la UNAM">
                 </a>
             </div>
             <div class="logo-enp">
-
-                <a href="http://enp.unam.mx/">
-
                 <a href="http://enp.unam.mx/" target="_blank">
-
                     <img class="iconos"src="../statics/img/logo_enp.jpeg" alt="Escuedo de la UNAM">
                 </a>
             </div>
             <div class="logo-enp6">
-
-                <a href="https://www.prepa6.unam.mx/ENP6/_P6/">
-
                 <a href="https://www.prepa6.unam.mx/ENP6/_P6/" target="_blank">
-
                     <img class="iconos"src="../statics/img/logo-prepa6.png" alt="Escuedo de la UNAM">
                 </a>
             </div>
@@ -118,20 +157,12 @@
         </div>
         <div id="iconos_ete">
             <div class="logo-compu">
-
-                <a href="https://www.ete.enp.unam.mx/">
-
                 <a href="https://www.ete.enp.unam.mx/" target="_blank">
-
                     <img class="iconos" src="../statics/img/logo_compu.jpeg" alt="Escudo de el Estudio Tecnico Especializado en Computacion">
                 </a>
             </div>
             <div class="logo-ete"></div>
-
-                <a href="https://www.ete.enp.unam.mx/CM.html">
-
                 <a href="https://www.ete.enp.unam.mx/CM.html" target="_blank">
-
                     <img class="iconos" src="../statics/img/logo-ete.png" alt="Escudo de los Estudios Tecnicos de la UNAM">
                 </a>
             </div>
@@ -140,61 +171,24 @@
             </div>
         </div>
     </header>
-
+    <!----------------------------BARRA DE NAVEGACIÓN----------------------------->
+    <nav class="nav">
+        <div class="container">
+            <nav class="menu">
+                <a href="./index.html">Inicio</a>
+                <a href="./inicio-sesion.php">Perfil</a>
+                <a href="./inicio-sesion.php">Acerca</a>
+                <a href="./inicio-sesion.php">Creditos</a>
+            </nav>
+        </div>
+    </nav>
+    <br>
     <!--Cuerpo después de la barra de busqueda-->
     <div class="cont-general">
         <div id="img-puma">
             <img id="img-inisesion" src="../statics/img/puma-telefono.jpg" alt="Puma con un teléfono">
         </div>
         <div id="form-inisesion">
-
-            <p>INICIO DE SESIÓN</p>
-            <!--Formulario de ingreso de datos-->
-            <form method="POST">
-                <p>Ingrese su usuario:</p>
-                <input type="text" placeholder="no. de cuenta">
-                <p>Ingrese su contraseña:</p>
-                <input type="text" placeholder="dd/mm/aaaa">
-                <br>
-                <button type="submit" id="envio-datos">Enviar</button>
-            </form method="POST">
-            <br>
-            <div>
-                <p class="tienes-cuenta">¿No tienes una cuenta?</p>
-                <p  class="crea-cuenta">Crea una</p>
-
-        <!------------------------BARRA DE NAVEGACIÓN------------------------>
-    <?php
-            include 'barrapro.php';
-    ?>
-    <!-------------------------------------BARRA LATERAL----------------------------------------->
-    <?php
-        include 'barra-lateral.php';
-    ?>
-    <div class="contenedor-general" style="margin-top: 50px;">
-        <header class="form-header">
-            <h2>Edición de foto de perfil</h2>
-        </header>
-            
-            <div class="vista-previa">
-                <?php
-                    $ruta_imagen="";
-                    if(file_exists("../statics/img/perfil-usuario.jpg")){
-                        $ruta_imagen= "../statics/img/perfil-usuario.jpg";
-                    }else{
-                        $ruta_imagen="../statics/img/imagen-predeterminada.jpeg";
-                    }
-                    echo "<img src= '$ruta_imagen' class= 'profile-pic'>";
-                ?>
-
-                <a href="perfil-alumno.php" class="btn-editar">Regresar al perfil</a>
-
-            </div>
-    </div>
-    <!------------------------FOOTER --------------------------------->
-    <?php
-            include 'footer.php';
-    ?>
             <br>
             <p>INICIO DE SESIÓN</p>
             <!--Formulario de ingreso de datos-->
@@ -210,5 +204,9 @@
             </form>
         </div>
     </div>
+    <!-----------------------------------FOOTER----------------------------------->
+    <?php
+            include 'footer.php';
+    ?> 
 </body>
 </html>
