c29f0d2f40e6bac9823d63d45859d0782d3eae51
Diego Ivan Salcedo Perez <325156992@alumno.enp.unam.mx>
2026-06-14 23:13:53 -0600

    fix(barrapro): arregle mis nacadas

--- templates/barrapro.php
+++ templates/barrapro.php
@@ -3,7 +3,29 @@
         <div class="btn-menu">
             <label for="btn-menu">☰</label>
         </div>
-        <nav class="menu">      
+        <nav class="menu">
+            <?php
+            session_start();
+            $ruta_inicio = "";
+            //var_dump($_SESSION);
+            //A, E, P
+            
+            switch ($_SESSION["tipo_usuario"]) {
+                case 'A':
+                    $ruta_inicio = './admin.php';
+                    break;
+                case 'E':
+                    $ruta_inicio = './alumno.php';
+                    break;
+                case 'P':
+                    $ruta_inicio = './profesor.php';
+                    break;
+                default:
+                    $ruta_inicio = "";
+                    break;
+            }
+            echo "";
+            ?>
             <a href="<?php echo $ruta_inicio ?>">Inicio</a>
             <a href="./perfil-alumno.php">Perfil</a>
             <a href="./acerca-de.php">Acerca</a>
